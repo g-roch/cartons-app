7629a122e61e034ad2e937a75c14159f3992f9d8
Gabriel Roch <gabriel@g-roch.ch>
2020-06-28 17:26:01 +0200

    Add header menu for standard page access

--- www/inc/header.php
+++ www/inc/header.php
@@ -1,18 +1,63 @@
-<!doctype html>
+<?php
+# vim: set shiftwidth=2 ts=2 expandtab softtabstop=2 ft=html:
+?><!doctype html>
 <html lang="fr">
-	<head>
-		<!-- Required meta tags -->
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
-			<!-- Bootstrap CSS -->
-		<link rel="stylesheet" href="css/bootstrap.min.css" />
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
 
-		<title>Cartons - app</title>
-	</head>
-	<body>
-<nav class="navbar sticky-top navbar-dark bg-dark">
-  <a class="navbar-brand" href="index.php">Cartons</a>
-</nav>
-		<div class="container-fluid">
+    <title>Cartons - app</title>
+  </head>
+  <body>
+    <!--
+      <nav class="navbar sticky-top navbar-dark bg-dark">
+      <a class="navbar-brand" href="index.php">Cartons</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      </div>
+
+
+      </nav>
+    -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+      <a class="navbar-brand" href="index.php">Cartons</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <!--
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+        -->
+        <ul class="navbar-nav mr-2">
+          <li class="nav-item">
+            <a class="nav-link" href="index.php">Accueil</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="types.php">Types</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Nouveau
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="carton.php">Carton</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <div class="container-fluid">
 
