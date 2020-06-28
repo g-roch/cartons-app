466fb01082bb98ff751471111b4b093a8a225712
Gabriel Roch <gabriel@g-roch.ch>
2020-06-28 17:30:08 +0200

    Add quantity of carton type in types.php

--- www/types.php
+++ www/types.php
@@ -3,7 +3,10 @@
 require_once 'inc/init.php';
 require 'inc/header.php';
 $data = $PDO->query( <<<sql
-SELECT * FROM `type`;
+SELECT `type`.*, COUNT(*) as quantity
+FROM `type`
+  LEFT JOIN `carton` ON `carton`.`type` = `type`.`id`
+GROUP BY `type`.`id`
 sql
 );
 ?>
@@ -16,6 +19,7 @@ sql
           <tr>
             <th scope="col">#</th>
             <th scope="col">Nom</th>
+            <th scope="col">Qte</th>
             <th scope="col">
               <div class="btn-group" role="group">
                 <a href="type.php?action=new" type="button" class="disabled btn btn-sm btn-outline-primary">Nouveau</a>
@@ -28,6 +32,7 @@ sql
           <tr>
             <th scope="row"><?= htmlentities($row['type.id']) ?></th>
             <td><?= htmlentities($row['type.name']) ?></td>
+            <td><?= htmlentities($row['.quantity']) ?></td>
             <td>
               <div class="btn-group" role="group">
                 <a href="carton.php?action=new&type=<?=htmlentities($row['type.id'])?>" type="button" class="btn btn-sm btn-outline-primary">Nouveau carton</a>
